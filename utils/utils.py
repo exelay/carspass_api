4b4b05bc07d34bd455a51e42906602378e92aa16
Alexey <exelay.rapik@gmail.com>
2020-12-30 12:45:23 +0300

    Added autoru, amru and avito endpoints.

--- utils/utils.py
+++ utils/utils.py
@@ -74,11 +74,11 @@ async def run_spider(
 ) -> str:
     site_config = await adapt_config(config, site)
     job = scrapyd.schedule(PROJECT_NAME, site,
-                     brand=brand, model=model, token=token,
-                     city=site_config['city'], radius=config['radius'],
-                     transmission=site_config['transmission'], price_min=config['price_min'],
-                     price_max=config['price_max'], year_min=config['year_min'], year_max=config['year_max'],
-                     v_min=site_config['v_min'], v_max=site_config['v_max'],
-                     steering_w=site_config['steering_w'], car_body=site_config['car_body'],
-                     vendor=site_config['vendor'], latest_ads=config['latest_ads'])
+                           brand=brand, model=model, token=token,
+                           city=site_config['city'], radius=config['radius'],
+                           transmission=site_config['transmission'], price_min=config['price_min'],
+                           price_max=config['price_max'], year_min=config['year_min'], year_max=config['year_max'],
+                           v_min=site_config['v_min'], v_max=site_config['v_max'],
+                           steering_w=site_config['steering_w'], car_body=site_config['car_body'],
+                           vendor=site_config['vendor'], latest_ads=config['latest_ads'])
     return job
